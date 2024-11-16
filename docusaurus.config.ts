73278751ff8652e8c876e5100728f8e478a82755
Andrea Mancuso <and.mancuso@googlemail.com>
2024-11-16 11:44:40 +0100

    Added link to Discord

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -98,23 +98,15 @@ const config: Config = {
             },
           ],
         },
-        // {
-        //   title: "Community",
-        //   items: [
-        //     {
-        //       label: "Stack Overflow",
-        //       href: "https://stackoverflow.com/questions/tagged/docusaurus",
-        //     },
-        //     {
-        //       label: "Discord",
-        //       href: "https://discordapp.com/invite/docusaurus",
-        //     },
-        //     {
-        //       label: "Twitter",
-        //       href: "https://twitter.com/docusaurus",
-        //     },
-        //   ],
-        // },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Discord",
+              href: "https://discord.gg/Cbgcajdq"
+            }
+          ],
+        },
         {
           title: "More",
           items: [
